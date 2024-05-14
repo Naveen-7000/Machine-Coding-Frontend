c1cd55d2d8fd39c349a0e9357ddd354cedbd4662
Naveen Bhusare <navinbhusare97n@gmail.com>
2024-05-14 11:43:40 +0530

    render c pattern div with click color change till last element and then recolor them in the same order they added into array

--- react+tailwindcss/src/App.jsx
+++ react+tailwindcss/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   const [cards, setCards] = useState([]);
   const [removeAll,setRemoveAll] = useState(false);
 
+
+  //  when the cards length reaches to 9 then mark the remove flag true
   const addItem = (item) =>{
     setCards([...cards,item]);
     if(item === 9)setRemoveAll(true)
@@ -28,7 +30,7 @@ function App() {
     <div className='bg-black w-full h-screen'>
       <div className='grid w-full grid-cols-3 justify-center items-center p-4 gap-4'>
          {
-          [1,2,3,4,5,6,7,8,9].map((item,index)=>{
+          [1,2,3,4,5,6,7,8,9].map((item)=>{
 
                return (
              <div key={item} className={`${cards.includes(item) ? 'bg-green-200' : 'bg-yellow-50'} w-10 h-10 ${item === 5 | item === 6 && 'bg-transparent text-transparent'} text-center cursor-pointer`} onClick={()=>{
